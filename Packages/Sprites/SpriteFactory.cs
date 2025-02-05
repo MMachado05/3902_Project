f3cf14caec773e57bba113d91dba45317c942e28
MMachado05 <marcialmachado0522@gmail.com>
2025-02-05 11:20:38 -0500

    Add TODO comment
    For future factories based on different kinds of characters

--- Packages/Sprites/SpriteFactory.cs
+++ Packages/Sprites/SpriteFactory.cs
@@ -2,4 +2,6 @@ namespace Project;
 
 public class SpriteFactory
 {
+    // TODO: Might be good to have multiple factories for different sprite types (e.g.
+    // enemies, characters, items, etc.)
 }
