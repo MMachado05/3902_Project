3687cb2f68180f557039b28723073a66e2fd56d9
MMachado05 <marcialmachado0522@gmail.com>
2025-02-07 16:52:55 -0500

    Create basic sprite classes

---
+++ Packages/Sprites/AnimatedSprite.cs
@@ -0,0 +1,20 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Project;
+
+public class AnimatedSprite : ISprite
+{
+    private Texture2D texture;
+    private int maxFrames;
+
+    public void Draw(SpriteBatch spriteBatch, Vector2 position)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Update()
+    {
+        throw new System.NotImplementedException();
+    }
+}
