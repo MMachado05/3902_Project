36bda3db88888a3aa69a46d58474c7088f1bad51
MMachado05 <marcialmachado0522@gmail.com>
2025-02-07 11:05:59 -0500

    Implement basic factory class
    Just the required fields and methods.

--- Packages/Sprites/SpriteFactory.cs
+++ Packages/Sprites/SpriteFactory.cs
@@ -1,7 +1,27 @@
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
 namespace Project;
 
 public class SpriteFactory
 {
     // TODO: Might be good to have multiple factories for different sprite types (e.g.
     // enemies, characters, items, etc.)
+
+    private Texture2D playerSpriteSheet;
+
+    private static SpriteFactory instance = new SpriteFactory();
+
+    public static SpriteFactory Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private SpriteFactory()
+    {
+    }
+
 }
