93811c7c8388e36ac04fc88f226890578a2fa60c
MMachado05 <marcialmachado0522@gmail.com>
2025-02-09 21:28:53 -0500

    Implement SingleAnimationSprite

--- Packages/Sprites/SingleAnimationSprite.cs
+++ Packages/Sprites/SingleAnimationSprite.cs
@@ -5,14 +5,41 @@ namespace Project
 {
     public class SingleAnimationSprite : AbstractSprite
     {
+        private int maxFrames;
+        private int currFrame;
+        private int topLeftXInitial;
+        private int topLeftYInitial;
+
+        /// <summary>
+        /// maxFrames is literal; if there are four frames of anmimation, pass in 4 as
+        /// the constructor argument.
+        /// </summary>
+        public SingleAnimationSprite(Texture2D texture, Rectangle sourceInitial, 
+            int scaleFactor, int maxFrames) : base(texture, sourceInitial, scaleFactor)
+        {
+            this.maxFrames = maxFrames - 1;
+            this.currFrame = 0;
+            this.topLeftXInitial = sourceInitial.X;
+            this.topLeftYInitial = sourceInitial.Y;
+        }
         public override void Draw(SpriteBatch spriteBatch, Vector2 position)
         {
-            throw new System.NotImplementedException();
+            Rectangle destRectangle = new Rectangle();
+            destRectangle.X = (int)(position.X - (base.widthPixels / 2)) * base.scaleFactor;
+            destRectangle.Y = (int)(position.Y - (base.heightPixels / 2)) * base.scaleFactor;
+            destRectangle.Width = (int)(position.X + (base.widthPixels / 2)) * base.scaleFactor;
+            destRectangle.Height = (int)(position.Y + (base.heightPixels / 2)) * base.scaleFactor;
+
+            spriteBatch.Draw(base.texture, destRectangle, base.source, Color.White);
         }
 
         public override void Update()
         {
-            throw new System.NotImplementedException();
+            if (this.currFrame < this.maxFrames)
+            {
+                this.currFrame++;
+                base.source.Y = this.topLeftYInitial + (base.heightPixels * this.currFrame);
+            }
         }
     }
 }
