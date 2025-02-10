7c6c56e93a6fc55a2996c38fbbd3e4517a73ee6d
MMachado05 <marcialmachado0522@gmail.com>
2025-02-09 20:55:17 -0500

    Add SingleAnimationSprite
    This will be used for sprites that only have one animation, like an
    attack.

---
+++ Packages/Sprites/SingleAnimationSprite.cs
@@ -0,0 +1,18 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Project
+{
+    public class SingleAnimationSprite : AbstractSprite
+    {
+        public override void Draw(SpriteBatch spriteBatch, Vector2 position)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Update()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
