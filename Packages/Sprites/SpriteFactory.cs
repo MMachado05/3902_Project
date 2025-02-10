93e707e6a62bf93f0089e2b4fc7e63a5bc099924
MMachado05 <marcialmachado0522@gmail.com>
2025-02-09 21:29:02 -0500

    Create undamaged player factory methods

--- Packages/Sprites/SpriteFactory.cs
+++ Packages/Sprites/SpriteFactory.cs
@@ -1,4 +1,5 @@
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace Project;
@@ -7,6 +8,8 @@ public class SpriteFactory
 {
     // TODO: Might be good to have multiple factories for different sprite types (e.g.
     // enemies, characters, items, etc.)
+    // TODO: Also I'm using way too many magic numbers, I should give these their own
+    // private fields to make modifying things easier.
 
     private Texture2D playerSpriteSheet;
 
@@ -26,11 +29,72 @@ public class SpriteFactory
 
     public void LoadAllTextures(ContentManager content)
     {
-        playerSpriteSheet = content.Load<Texture2D>("Character32x32TextureTemplate"); // TODO: Replace with actual
+        this.playerSpriteSheet = content.Load<Texture2D>("Character32x32TextureTemplate"); // TODO: Replace with actual
     }
 
-    public ISprite CreatePlayerSprite()
+    // Stopped Player sprites
+    public ISprite NewUpStoppedPlayer()
     {
+      return new StationarySprite(this.playerSpriteSheet,
+          new Rectangle(0, 0, 32, 32), 32, 32, 2);
+    }
+    public ISprite NewRightStoppedPlayer()
+    {
+      return new StationarySprite(this.playerSpriteSheet,
+          new Rectangle(32, 0, 32, 32), 32, 32, 2);
+    }
+    public ISprite NewDownStoppedPlayer()
+    {
+      return new StationarySprite(this.playerSpriteSheet,
+          new Rectangle(64, 0, 32, 32), 32, 32, 2);
+    }
+    public ISprite NewLeftStoppedPlayer()
+    {
+      return new StationarySprite(this.playerSpriteSheet,
+          new Rectangle(96, 0, 32, 32), 32, 32, 2);
+    }
 
+    // Walking Player sprites
+    public ISprite NewUpWalkingPlayer()
+    {
+      return new AnimatedLoopSprite(this.playerSpriteSheet,
+          new Rectangle(0, 32, 32, 32), 2, 4);
+    }
+    public ISprite NewRightWalkingPlayer()
+    {
+      return new AnimatedLoopSprite(this.playerSpriteSheet,
+          new Rectangle(32, 32, 32, 32), 2, 4);
+    }
+    public ISprite NewDownWalkingPlayer()
+    {
+      return new AnimatedLoopSprite(this.playerSpriteSheet,
+          new Rectangle(64, 32, 32, 32), 2, 4);
+    }
+    public ISprite NewLeftWalkingPlayer()
+    {
+      return new AnimatedLoopSprite(this.playerSpriteSheet,
+          new Rectangle(96, 32, 32, 32), 2, 4);
+    }
+
+    // Attacking Player sprites - single use
+    public ISprite NewUpAttackingPlayer()
+    {
+      return new SingleAnimationSprite(this.playerSpriteSheet,
+          new Rectangle(0, 160, 32, 64), 2, 4);
+    }
+    public ISprite NewRightAttackingPlayer()
+    {
+      return new SingleAnimationSprite(this.playerSpriteSheet,
+          new Rectangle(64, 160, 64, 32), 2, 4);
+    }
+    public ISprite NewDownAttackingPlayer()
+    {
+      return new SingleAnimationSprite(this.playerSpriteSheet,
+          new Rectangle(32, 160, 32, 64), 2, 4);
+    }
+    public ISprite NewLeftAttackingPlayer()
+    {
+      return new SingleAnimationSprite(this.playerSpriteSheet,
+          new Rectangle(64, 288, 64, 32), 2, 4);
     }
 }
