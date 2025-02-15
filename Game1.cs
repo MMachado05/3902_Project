a6fe50db35036c784de354522778ec1d5e1a5a40
baqer-issa <bakerissa2015@gmail.com>
2025-02-15 06:38:46 -0500

    Load solidblocks and implements nextBlock Mehtod

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Project.Blocks;
 
 namespace Project;
 
@@ -8,6 +9,26 @@ public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+    public SolidBlock activeBlock;
+    public NextBlockCommand nextBlockCommand;
+    public KeyboardState input;
+    public KeyboardState previous;
+    SolidBlockController solidBlockController;
+    SolidBlock [] arrayOfSolidBlock;
+    Texture2D blockSheet;
+    Rectangle destination;
+    int position;
+
+        public void nextBlock()
+         {
+             if(position<arrayOfSolidBlock.Length-1){
+                 position++;
+                 activeBlock = arrayOfSolidBlock[position];
+             }else{
+                 activeBlock=arrayOfSolidBlock[0];
+                 position=0;
+             }
+         }
 
     public Game1()
     {
@@ -19,14 +40,35 @@ public class Game1 : Game
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
+        position=0;
+        input = Keyboard.GetState();
+        previous = Keyboard.GetState();
 
+        arrayOfSolidBlock = new SolidBlock [4];
         base.Initialize();
     }
 
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
+         blockSheet = Content.Load<Texture2D>("SolidBlocks32x32");
+        destination= new Rectangle(0,0,32,32);
+         Rectangle a = new Rectangle(0,0,32,32);
+        Rectangle b = new Rectangle(32,0,32,32);
+        Rectangle c= new Rectangle(64,0,32,32);
+        Rectangle d= new Rectangle(96,0,32,32);
+      SolidBlock blocksa = new SolidBlock(_spriteBatch,blockSheet,destination,a) ;
+        SolidBlock blocksb = new SolidBlock(_spriteBatch,blockSheet,destination,b) ;
+        SolidBlock blocksc = new SolidBlock(_spriteBatch,blockSheet,destination,c) ;
+        SolidBlock blocksd = new SolidBlock(_spriteBatch,blockSheet,destination,d) ;
+         arrayOfSolidBlock[0]= blocksa;
+        arrayOfSolidBlock[1]= blocksb;
+        arrayOfSolidBlock[2]= blocksc;
+        arrayOfSolidBlock[3]= blocksd;
+        activeBlock = arrayOfSolidBlock[0];
 
+       nextBlockCommand = new NextBlockCommand(this);
+        solidBlockController = new SolidBlockController(this,nextBlockCommand);
         // TODO: use this.Content to load your game content here
     }
 
@@ -34,6 +76,12 @@ public class Game1 : Game
     {
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
+             input = Keyboard.GetState();
+       
+       solidBlockController.ProcessControls();
+       if(!(input.Equals(previous))){
+            previous = input;
+       }
 
         // TODO: Add your update logic here
 
@@ -45,6 +93,9 @@ public class Game1 : Game
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
         // TODO: Add your drawing code here
+        _spriteBatch.Begin();
+        activeBlock.Draw();
+        _spriteBatch.End();
 
         base.Draw(gameTime);
     }
