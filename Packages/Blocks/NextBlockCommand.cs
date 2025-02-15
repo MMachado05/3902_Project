795bdd862683a83b7aeae7289aaa6a34945f62c1
baqer-issa <bakerissa2015@gmail.com>
2025-02-15 03:06:03 -0500

    create NextBlockCommand, PreviousBlockCommand and SolidBlockController

---
+++ Packages/Blocks/NextBlockCommand.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Blocks
+{
+    public class NextBlockCommand : ICommand
+    {
+        public void Execute()
+        {
+        }
+    }
+}
