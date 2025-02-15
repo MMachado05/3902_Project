2f0d16d66c2926ea919cbcb29e2a4e5b736950a7
baqer-issa <bakerissa2015@gmail.com>
2025-02-15 07:27:44 -0500

    Implement PreviousBlockCommand

--- Packages/Blocks/PreviousBlockCommand.cs
+++ Packages/Blocks/PreviousBlockCommand.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace 3902_Project.Packages.Blocks
+namespace Project.Blocks
 {
-    public class PreviousBlockCommand
+    public class PreviousBlockCommand : ICommand
     {
-        
+         private Game1 game1;
+         public PreviousBlockCommand(Game1 game)
+        {
+            this.game1 = game;
+           
+        }
+        public void Execute()
+        {
+            game1.previousBlock();
+        }
     }
 }
