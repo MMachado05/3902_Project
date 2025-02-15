ab42ad71983f90e0b9c4bb99f24abd16452a3bdd
baqer-issa <bakerissa2015@gmail.com>
2025-02-15 06:30:18 -0500

    Implements SolidBlockControler

--- Packages/Blocks/SolidBlockController.cs
+++ Packages/Blocks/SolidBlockController.cs
@@ -2,14 +2,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input;
 
 namespace Project.Blocks
 {
     public class SolidBlockController : IController
     {
+        private NextBlockCommand NextBlock;
+        private PreviousBlockCommand PreviousBlock;
+
+       // private KeyboardState input;
+        bool up;
+        Game1 _game;
+         public SolidBlockController(Game1 game, NextBlockCommand nextBlock)
+        {
+            this.up = false;
+            NextBlock = nextBlock;
+            //PreviousBlock = previousBlock;
+            _game = game;
+        }
 
         public void ProcessControls()
         {
+              if((_game.input.IsKeyDown(Keys.NumPad2)|| _game.input.IsKeyDown(Keys.D2))&& !_game.input.Equals(_game.previous)){
+               NextBlock.Execute();
+                }
         }
     }
 }
