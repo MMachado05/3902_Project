d00026a058ef2a0cd60224170c2d66e79880a784
baqer-issa <bakerissa2015@gmail.com>
2025-02-15 06:26:32 -0500

    implements nextBlockCommand class

--- Packages/Blocks/NextBlockCommand.cs
+++ Packages/Blocks/NextBlockCommand.cs
@@ -7,8 +7,15 @@ namespace Project.Blocks
 {
     public class NextBlockCommand : ICommand
     {
+        private Game1 game1;
+         public NextBlockCommand(Game1 game)
+        {
+            this.game1 = game;
+           
+        }
         public void Execute()
         {
+            game1.nextBlock();
         }
     }
 }
