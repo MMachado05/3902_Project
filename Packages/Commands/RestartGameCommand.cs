dbee93fe289a5d219993b9f0238d4faad47ec602
baqer-issa <bakerissa2015@gmail.com>
2025-02-17 14:39:00 -0500

    Create a restart Command

--- Packages/Commands/RestartGameCommand.cs
+++ Packages/Commands/RestartGameCommand.cs
@@ -9,18 +9,19 @@ namespace Project.Commands
     {
         private Game1 _game;
         private SpriteBatch spriteBatch;
-        private RestartGameCommand(Game1 game, SpriteBatch spriteBatch)
+        public static RestartGameCommand instance;
+        public RestartGameCommand(Game1 game, SpriteBatch spriteBatch)
         {
             _game = game;
             this.spriteBatch = spriteBatch;
+
         }
 
         public void Execute()
         {
 
-            _game.player = new Player();
-            _game.enemyManager = new EnemyManager();
-            _game.manager = new SolidBlockManager(spriteBatch);
+            _game.restart();
+
 
         }
     }
