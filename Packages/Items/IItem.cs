832fe4d8737de75427f57f3b9590b9c505099b26
MMachado05 <marcialmachado0522@gmail.com>
2025-02-23 17:30:04 -0500

    Create summaries for all interfaces

--- Packages/Items/IItem.cs
+++ Packages/Items/IItem.cs
@@ -7,6 +7,9 @@ namespace Project
     {
         Vector2 Position { get; set; }
         float Speed { get; set; }
+        /// <summary>
+        /// Draw this item to the game window.
+        /// </summary>
         void Draw(SpriteBatch spriteBatch);
     }
 }
