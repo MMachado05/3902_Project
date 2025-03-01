5b9b30263d8fa0f85354088800073d2d77af2738
baqer-issa <bakerissa2015@gmail.com>
2025-03-01 07:11:41 -0500

    reimplement levelOneroom based on csv file

--- Packages/rooms/LevelOneRoom.cs
+++ Packages/rooms/LevelOneRoom.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using Microsoft.Xna.Framework;
 using Project.Blocks;
 using Project.rooms;
 namespace Project.Packages
@@ -6,11 +9,32 @@ namespace Project.Packages
     public class LevelOneRoom : IRoom
     {
         SolidBlockManager blocks;
+        private Dictionary<Vector2,String> room;
+        RoomParser parser;
         public LevelOneRoom( SolidBlockManager blocks)
         {
             this.blocks = blocks;
+            parser = new RoomParser();
+            room = parser.loadRoom("../../../Data/room1.csv");
+            
+        }
+        public List<SolidBlock> roomMap(){
+            List <SolidBlock> result = new List<SolidBlock>();
+            foreach(var item in room){
+                Rectangle dest = new ((int)item.Key.X*32,(int)item.Key.Y*32,32,32);
+                SolidBlock block =blocks.boardersBrick(dest);
+                result.Add(block);
+            }
+            return result ;
         }
         public void Draw(){
+            List<SolidBlock> blockslist = roomMap();
+
+            foreach(var block in blockslist){
+                block.Draw();
+            }
+
+            /*
             List <SolidBlock> blocksList = blocks.topBoarders();
             foreach(var block in blocksList){
                 block.Draw();
@@ -30,7 +54,7 @@ namespace Project.Packages
             blocksList = blocks.obstacles();
              foreach(var block in blocksList){
                 block.Draw();
-            }
+            }*/
         }
         
       
