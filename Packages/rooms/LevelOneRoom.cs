ea2a99aa8034711777f558959bb9833aa45db3d1
baqer-issa <bakerissa2015@gmail.com>
2025-03-01 16:44:24 -0500

    implement drawing method to handle all objects

--- Packages/rooms/LevelOneRoom.cs
+++ Packages/rooms/LevelOneRoom.cs
@@ -1,62 +1,70 @@
 using System;
 using System.Collections.Generic;
 using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
 using Microsoft.Xna.Framework;
 using Project.Blocks;
+using Project.Enemies;
+using Project.Packages.Items;
 using Project.rooms;
 namespace Project.Packages
 {
     public class LevelOneRoom : IRoom
     {
         SolidBlockManager blocks;
-        private Dictionary<Vector2,String> room;
+        private Dictionary<Vector2, String> room;
         RoomParser parser;
-        public LevelOneRoom( SolidBlockManager blocks)
+        public LevelOneRoom(SolidBlockManager blocks)
         {
             this.blocks = blocks;
             parser = new RoomParser();
             room = parser.loadRoom("../../../Data/room1.csv");
-            
+
         }
-        public List<SolidBlock> roomMap(){
-            List <SolidBlock> result = new List<SolidBlock>();
-            foreach(var item in room){
-                Rectangle dest = new ((int)item.Key.X*32,(int)item.Key.Y*32,32,32);
-                SolidBlock block =blocks.boardersBrick(dest);
+        public List<Object> roomMap()
+        {
+            List<object> result = new List<object>();
+            foreach (var item in room)
+            {
+                Rectangle dest = new((int)item.Key.X * 64, (int)item.Key.Y * 64, 64, 64);
+                SolidBlock block = blocks.boardersBrick(dest);
                 result.Add(block);
             }
-            return result ;
+            return result;
         }
-        public void Draw(){
-            List<SolidBlock> blockslist = roomMap();
+        public void Draw()
+        {
+            List<object> itemList = roomMap();
 
-            foreach(var block in blockslist){
-                block.Draw();
+            foreach (var items in itemList)
+            {
+                switch (items)
+                {
+                    case SolidBlock solidBlock:
+                        solidBlock.Draw();
+                        break;
+                    case Player player:
+                        Console.WriteLine("p");
+                        break;
+                    case IItem item:
+                        Console.WriteLine("I");
+
+                        // nothing 
+                        break;
+                    case IEnemy:
+                        Console.WriteLine("E");
+
+                        // nothing
+                        break;
+                    default:
+
+                        break;
+                }
+
             }
 
-            /*
-            List <SolidBlock> blocksList = blocks.topBoarders();
-            foreach(var block in blocksList){
-                block.Draw();
-            }
-            blocksList = blocks.bottomBoarders();
-             foreach(var block in blocksList){
-                block.Draw();
-            }
-            blocksList = blocks.rightBoarders();
-             foreach(var block in blocksList){
-                block.Draw();
-            }
-            blocksList = blocks.leftBoarders();
-             foreach(var block in blocksList){
-                block.Draw();
-            }
-            blocksList = blocks.obstacles();
-             foreach(var block in blocksList){
-                block.Draw();
-            }*/
         }
-        
-      
+
+
     }
 }
