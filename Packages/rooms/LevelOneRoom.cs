18e35282eddbd3ad6c65808a7dbc7cd1f47a2341
baqer-issa <bakerissa2015@gmail.com>
2025-03-02 03:33:08 -0500

    recreate level 1 room to extend baseRoom

--- Packages/rooms/LevelOneRoom.cs
+++ Packages/rooms/LevelOneRoom.cs
@@ -1,95 +1,19 @@
 using System;
 using System.Collections.Generic;
-using System.Reflection.Metadata.Ecma335;
-using System.Runtime.CompilerServices;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Project.Blocks;
-using Project.Enemies;
-using Project.Packages.Items;
-using Project.rooms;
-namespace Project.Packages
+using Project.Packages;
+
+namespace Project.rooms
 {
-    public class LevelOneRoom : IRoom
+    public class LevelOneRoom : BaseRoom
     {
-        SolidBlockManager blocks;
-        private Dictionary<Vector2, String> room;
-        RoomParser parser;
-        public LevelOneRoom(SolidBlockManager blocks)
-        {
-            this.blocks = blocks;
-            parser = new RoomParser();
-            room = parser.loadRoom("../../../Data/room1.csv");
 
+        public LevelOneRoom(SolidBlockManager blocks) : base(blocks){
+            
         }
-        public List<Object> roomMap()
-        {
-            List<object> result = new List<object>();
-            foreach (var item in room)
-            {
-                Rectangle dest;
-                SolidBlock block;
-
-                switch (item.Value)
-                {
-                    case "bl":
-                        dest = new((int)item.Key.X * 64, (int)item.Key.Y * 64, 64, 64);
-                        block = blocks.boardersBrick(dest);
-                        result.Add(block);
-                        break;
-                    case "ob":
-                        dest = new((int)item.Key.X * 64, (int)item.Key.Y * 64, 64, 64);
-                        block = blocks.obstacleBlock(dest);
-                        result.Add(block);
-
-
-                        break;
-                    case "dr":
-                        dest = new((int)item.Key.X * 64, (int)item.Key.Y * 64, 64, 64);
-                        block = blocks.doorBlock(dest);
-                        result.Add(block);
-                        break;
-                    case "en":
-                        break;
-                    case "pl":
-                        break;
-                }
-
-            }
-            return result;
-        }
-        public void Draw()
-        {
-            List<object> itemList = roomMap();
-
-            foreach (var items in itemList)
-            {
-                switch (items)
-                {
-                    case SolidBlock solidBlock:
-                        solidBlock.Draw();
-                        break;
-                    case Player player:
-                        Console.WriteLine("p");
-                        break;
-                    case IItem item:
-                        Console.WriteLine("I");
-
-                        // nothing 
-                        break;
-                    case IEnemy:
-                        Console.WriteLine("E");
-
-                        // nothing
-                        break;
-                    default:
-
-                        break;
-                }
-
-            }
-
-        }
-
-
+        
     }
 }
