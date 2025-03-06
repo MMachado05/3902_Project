6ebe9c9d4d55b6c5a2315e93dee486abb66a23b6
Osama-Abuhilal <OsamaTheLama2.0@gmail.com>
2025-03-05 22:28:20 -0500

    Refactored and exposed public properties for player instance variables to move more logic into collision manager

--- Packages/Commands/PlayerBlockCollisionCommand.cs
+++ Packages/Commands/PlayerBlockCollisionCommand.cs
@@ -1,19 +1,22 @@
 ﻿using Project.Blocks;
 using Project;
+using Project.Packages.Characters;
 
 public class PlayerBlockCollisionCommand : ICommand
 {
     private Player _player;
     private SolidBlock _block;
+    private CollisionManager _collisionManager;
 
-    public PlayerBlockCollisionCommand(Player player, SolidBlock block)
+    public PlayerBlockCollisionCommand(Player player, SolidBlock block, CollisionManager collisionManager)
     {
         _player = player;
         _block = block;
+        _collisionManager = collisionManager;
     }
 
     public void Execute()
     {
-        _player.HandleBlockCollision(_block);
+        _collisionManager.HandleBlockCollision(_player, _block);
     }
 }
