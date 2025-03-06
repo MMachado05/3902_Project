e2f97e3bcc2d850ded8ad3358b799ded985fb6c2
Osama-Abuhilal <OsamaTheLama2.0@gmail.com>
2025-03-06 14:58:55 -0500

    offsets removed from player

--- Packages/Characters/CollisionManager.cs
+++ Packages/Characters/CollisionManager.cs
@@ -13,7 +13,7 @@ namespace Project.Packages.Characters
             // NOTE: Because the sprite is not actually drawn exactly where the bounding box is, we have to do these weird offsets.
             // This can be fixed by fixing the sprite textures for the blocks at some point. Then the offsets can be removed.
             player.PositionVector = player.PreviousPosition;
-            player.PositionRect = new Rectangle((int)player.PreviousPosition.X - 32, (int)player.PreviousPosition.Y - 32,
+            player.PositionRect = new Rectangle((int)player.PreviousPosition.X, (int)player.PreviousPosition.Y,
                                          player.PositionRect.Width, player.PositionRect.Height);
 
             // Can prob add reduce health logic here later; trigger damage animation
