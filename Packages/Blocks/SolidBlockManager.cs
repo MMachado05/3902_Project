b6f30fc4cc19dfbf8e2c2b29f515ee2087f0962b
MMachado05 <marcialmachado0522@gmail.com>
2025-03-17 11:36:05 -0400

    Formatting

--- Packages/Blocks/SolidBlockManager.cs
+++ Packages/Blocks/SolidBlockManager.cs
@@ -5,21 +5,15 @@ namespace Project.Blocks
 {
     public class SolidBlockManager
     {
-
         SpriteBatch _SpriteBatch;
         Texture2D test;
-
-
 
         public SolidBlockManager(SpriteBatch spriteBatch)
         {
             _SpriteBatch = spriteBatch;
             this.test = SolidBlockSpriteFactory.Instance.getSolidBlockSheet();
-
-
-
-
         }
+
         public SolidBlock boardersBrick(Rectangle destination)
         {
             return new SolidBlock(_SpriteBatch, test, SolidBlockSpriteFactory.Instance.boardersBrick(), destination);
@@ -32,12 +26,5 @@ namespace Project.Blocks
         {
             return new SolidBlock(_SpriteBatch, test, SolidBlockSpriteFactory.Instance.doorBlock(), destination);
         }
-
-        // Added in order to allow game1.cs to iterate over all blocks, rather than just the current one.
-        /* public List<SolidBlock> GetAllBlocks()
-         {
-             return Blocks;
-         }*/
-
     }
 }
