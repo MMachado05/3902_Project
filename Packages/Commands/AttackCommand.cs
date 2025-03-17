0f7907578d93c3cf7824bd383630572d341a13fe
MMachado05 <marcialmachado0522@gmail.com>
2025-03-17 11:22:28 -0400

    Use Direction enum for Attacking command

--- Packages/Commands/AttackCommand.cs
+++ Packages/Commands/AttackCommand.cs
@@ -12,39 +12,8 @@
         public void Execute()
         {
 
-            string direction = _player.LastDirection; // Must be in Excecute for lastest direction, NOT constructor
-
             if (_player.Sprite.State != SpriteState.Attacking)
-            {
-                switch (direction)
-                {
-                    case "Up":
-                        if (_player.isDamaged)
-                            _player.ChangeSprite(PlayerSpriteFactory.Instance.NewDamagedUpAttackingPlayer());
-                        else
-                            _player.ChangeSprite(PlayerSpriteFactory.Instance.NewUpAttackingPlayer());
-                        break;
-                    case "Down":
-                        if (_player.isDamaged)
-                            _player.ChangeSprite(PlayerSpriteFactory.Instance.NewDamagedDownAttackingPlayer());
-                        else
-                            _player.ChangeSprite(PlayerSpriteFactory.Instance.NewDownAttackingPlayer());
-                        break;
-                    case "Left":
-                        if (_player.isDamaged)
-                            _player.ChangeSprite(PlayerSpriteFactory.Instance.NewDamagedLeftAttackingPlayer());
-                        else
-                            _player.ChangeSprite(PlayerSpriteFactory.Instance.NewLeftAttackingPlayer());
-                        break;
-                    case "Right":
-                        if (_player.isDamaged)
-                            _player.ChangeSprite(PlayerSpriteFactory.Instance.NewDamagedRightAttackingPlayer());
-                        else
-                            _player.ChangeSprite(PlayerSpriteFactory.Instance.NewRightAttackingPlayer());
-                        break;
-                }
-
-            }
+              _player.ChangeSprite(PlayerSpriteFactory.Instance.NewAttackingPlayerSprite(_player.LastDirection, _player.isDamaged));
 
         }
     }
