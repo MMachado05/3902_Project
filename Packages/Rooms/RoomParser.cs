0cbcf572da5496d46a7fc17eea817a2599b640f1
MMachado05 <marcialmachado0522@gmail.com>
2025-03-17 14:09:00 -0400

    Formatting

--- Packages/Rooms/RoomParser.cs
+++ Packages/Rooms/RoomParser.cs
@@ -6,44 +6,48 @@ using Microsoft.Xna.Framework;
 namespace Project.rooms
 {
     public class RoomParser
-    {         
-         public Dictionary <Vector2,String> loadRoom(string filepath){
-            Dictionary<Vector2,String> result = new ();
-            StreamReader reader = new (filepath);
+    {
+        public Dictionary<Vector2, String> loadRoom(string filepath)
+        {
+            Dictionary<Vector2, String> result = new();
+            StreamReader reader = new(filepath);
             string line;
-            int y=0;
-            while((line = reader.ReadLine())!=null){
-                string [] items = line.Split(',');
-                for(int x=0;x<items.Length;x++){
-                    switch(items[x]){
+            int y = 0;
+            while ((line = reader.ReadLine()) != null)
+            {
+                string[] items = line.Split(',');
+                for (int x = 0; x < items.Length; x++)
+                {
+                    switch (items[x])
+                    {
                         case "bl":
-                        result[new Vector2(x,y)] = items[x];
-                        break;
+                            result[new Vector2(x, y)] = items[x];
+                            break;
                         case "ob":
-                        result[new Vector2(x,y)] = items[x];
-                        break;
+                            result[new Vector2(x, y)] = items[x];
+                            break;
                         case "dr":
-                        result[new Vector2(x,y)] = items[x];
-                        break;
+                            result[new Vector2(x, y)] = items[x];
+                            break;
                         case "pl":
-                        result[new Vector2(x,y)] = items[x];
-                        break;
+                            result[new Vector2(x, y)] = items[x];
+                            break;
                         case "en":
-                        result[new Vector2(x,y)] = items[x];
-                        break;
+                            result[new Vector2(x, y)] = items[x];
+                            break;
 
                         default:
-                        break;
+                            break;
                     }
                 }
                 y++;
 
             }
-            
-            
+
+
             return result;
         }
 
-        
+
     }
 }
