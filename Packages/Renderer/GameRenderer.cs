44cf00c3bd73b3e7a9e6db5de96852db7bf716b1
MMachado05 <marcialmachado0522@gmail.com>
2025-03-24 10:39:15 -0400

    Implement room-drawing functionality in GameRenderer

--- Packages/Renderer/GameRenderer.cs
+++ Packages/Renderer/GameRenderer.cs
@@ -7,16 +7,49 @@ namespace Project.renderer
     public class GameRenderer : IRenderer
     {
         private Player _playerCharacter;
-        private RoomManager _roomManager;
+        public Player PlayerCharacter { set => this._playerCharacter = value; }
 
-        public GameRenderer() { }
+        private RoomManager _roomManager;
+        public RoomManager RoomManager { set => this._roomManager = value; }
+
+        private bool _fieldsSatisfied;
+
+        private int tileWidth;
+        private int tileHeight;
+
+        public int TileWidth { get => tileWidth; }
+        public int TileHeight { get => tileHeight; }
+
+        public GameRenderer(int tileWidth, int tileHeight)
+        {
+            this.tileWidth = tileWidth;
+            this.tileHeight = tileHeight;
+            this._fieldsSatisfied = false;
+        }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            throw new System.NotImplementedException();
+            // Ensure dependencies have been injected
+            if (!this._fieldsSatisfied)
+                this.SatisfyFields();
+
+            this._roomManager.DrawCurrentRoom(spriteBatch);
+            this._playerCharacter.Draw(spriteBatch);
+
+            // TODO: Catch collisions during the drawing stage and call relevant commands
+            //  to colliding objects as needed
         }
 
         /// <summary>The renderer shouldn't need to update anything.</summary>
         public void Update(GameTime gameTime) { }
+
+        private void SatisfyFields()
+        {
+            if (this._playerCharacter == null)
+                throw new System.NullReferenceException("Player property must be set.");
+            if (this._roomManager == null)
+                throw new System.NullReferenceException("RoomManager property must be set.");
+            this._fieldsSatisfied = true;
+        }
     }
 }
