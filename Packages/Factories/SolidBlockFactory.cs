8007333fd9008bb0d53668a1693742d4138569bd
MMachado05 <marcialmachado0522@gmail.com>
2025-03-24 10:38:51 -0400

    Refactor blocks to be built by a factory

---
+++ Packages/Factories/SolidBlockFactory.cs
@@ -0,0 +1,55 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Project.Blocks
+{
+    public class SolidBlockFactory
+    {
+        private Texture2D _textureAtlas;
+
+        private static SolidBlockFactory instance = new SolidBlockFactory();
+
+        public static SolidBlockFactory Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        private SolidBlockFactory()
+        {
+        }
+
+        public void LoadAllTextures(ContentManager content)
+        {
+            this._textureAtlas = content.Load<Texture2D>("blocks");
+        }
+
+        /// <summary>
+        /// Creates a set of blocks with a brick texture. Select the number of individual
+        /// blocks using the "horizontals" and "verticals" parameters.
+        /// </summary>
+        public IBlock CreateBricks(int horizontals, int verticals, Rectangle dest)
+        {
+            return new SolidBlock(_textureAtlas,
+                new Rectangle(320, 256, 64, 64),
+                horizontals, verticals, dest);
+        }
+
+        public IBlock CreateDoor(Rectangle dest)
+        {
+            return new SolidBlock(_textureAtlas,
+                new Rectangle(384, 256, 64, 64),
+                1, 1, dest);
+        }
+
+        public IBlock CreateWoodPlanks(int horizontals, int verticals, Rectangle dest)
+        {
+            return new SolidBlock(_textureAtlas,
+                new Rectangle(384, 384, 64, 64),
+                horizontals, verticals, dest);
+        }
+    }
+}
