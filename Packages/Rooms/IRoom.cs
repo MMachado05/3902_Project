86fc990f163340f82caa3c3f7ff75c6ca5968f35
MMachado05 <marcialmachado0522@gmail.com>
2025-03-27 16:14:10 -0400

    Add missing methods to interface

--- Packages/Rooms/IRoom.cs
+++ Packages/Rooms/IRoom.cs
@@ -1,10 +1,15 @@
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace Project.rooms
 {
     public interface IRoom
     {
+        public Rectangle PlayerLocation { get; }
+        public Rectangle SavedPlayerLocation { set; }
+        public bool IsOnScreen { get; set; }
+
         public void Draw(SpriteBatch sb);
-        public int getPlayerIndex();
+        public void AssignPlayer(Player player);
     }
 }
