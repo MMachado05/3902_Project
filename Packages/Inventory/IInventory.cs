073051c01fd1ea8eb632a07502a1d6667d997c29
baqer-issa <bakerissa2015@gmail.com>
2025-03-29 19:36:37 -0400

    creats Inventory Interface

---
+++ Packages/Inventory/IInventory.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace 3902_Project.Packages.Inventory
+{
+    public interface IInventory
+    {
+        
+    }
+}
