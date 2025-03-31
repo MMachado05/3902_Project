c6384600bdb69a51092161e26c3e79a83280fe81
MMachado05 <marcialmachado0522@gmail.com>
2025-03-31 12:16:24 -0400

    Change to fit new player velocity system

--- Packages/Commands/PlayerCommands/UpdateVelocityCommand.cs
+++ Packages/Commands/PlayerCommands/UpdateVelocityCommand.cs
@@ -8,16 +8,18 @@ namespace Project.Commands.PlayerCommands
     {
         private Player _player;
         private Direction _direction;
+        private bool _deregister;
         private int _dx;
         private int _dy;
         private bool _useX;
         private bool _useY;
 
-        public UpdateVelocityCommand(Player player, Direction direction,
+        public UpdateVelocityCommand(Player player, Direction direction, bool deregister,
             int dx, int dy, bool useX, bool useY)
         {
             _direction = direction;
             _player = player;
+            this._deregister = deregister;
             this._dx = dx;
             this._dy = dy;
             this._useX = useX;
@@ -30,15 +32,16 @@ namespace Project.Commands.PlayerCommands
             if (_player.Sprite.State == CharacterState.Attacking)
                 return;
 
-            if (_player.Sprite.State != CharacterState.Walking
-                || (_player.Sprite.State == CharacterState.Walking
-                  && _direction != _player.LastDirection))
-                _player.ChangeSprite(PlayerSpriteFactory.Instance.NewWalkingPlayerSprite(_direction, _player.isDamaged));
+            // Register direction for associated velocity change
+            if (!this._deregister)
+                this._player.RegisterDirection(this._direction, this._dx, this._dy);
+            else
+                this._player.DeregisterDirection(this._direction);
+
+            _player.ChangeSprite(PlayerSpriteFactory.Instance.NewWalkingPlayerSprite(this._player.LastActiveDirection, _player.isDamaged));
 
             _player.SpriteType = _direction;
             _player.Sprite.State = CharacterState.Walking;
-
-            _player.UpdateVelocity(_dx, _dy, _useX, _useY, _direction);
         }
     }
 }
