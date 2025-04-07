5416628984d581f05d7473f2ebbc4a0c32886f4e
MMachado05 <marcialmachado0522@gmail.com>
2025-04-07 13:23:28 -0400

    Create skeleton for BasicAttack item

---
+++ Packages/Items/BasicAttack.cs
@@ -0,0 +1,20 @@
+using Microsoft.Xna.Framework;
+using Project.Sprites;
+
+namespace Project.Items
+{
+    public class BasicAttack : Item
+    {
+        public BasicAttack(ISprite sprite) : base(sprite)
+        {
+        }
+
+        public override float Speed { get; set; }
+        public override Rectangle Location { get; set; }
+
+        public override void Update()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
